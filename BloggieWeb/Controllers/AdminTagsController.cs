39ad13f8ea431034c6fedfce8b7662c158bbcb42
Shivam <shivamkhantwal9410@gmail.com>
2023-06-13 16:38:28 +0530

    basic completed

---
+++ BloggieWeb/Controllers/AdminTagsController.cs
@@ -0,0 +1,46 @@
+﻿using BloggieWeb.Data;
+using BloggieWeb.Models.Domain;
+using BloggieWeb.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace BloggieWeb.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+
+        private BloggieDbContext _bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext) {
+          
+        _bloggieDbContext=bloggieDbContext;
+       
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+       
+        }
+
+        [HttpPost]
+
+        [ActionName("Add")]
+        public IActionResult
+            Add(AddTagRequest addTagRequest)
+        {
+            //mapping addtorequst to tag domain model
+            var tag = new Tag
+        { Name = addTagRequest.Name,
+          DisplayName = addTagRequest.DisplayName
+        
+          };
+
+            _bloggieDbContext.Tags.Add(tag);
+            _bloggieDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
---
+++ BloggieWeb/Data/BloggieDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BloggieWeb.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggieWeb.Data
+{
+    public class BloggieDbContext :DbContext
+    {
+       public BloggieDbContext(DbContextOptions<BloggieDbContext> options) : base(options) {
+      
+            
+            options.
+
+        
+        
+        }
+
+        public  DbSet<BlogPost>BlogPosts { get; set; }
+        public  DbSet<Tag> Tags { get; set; }
+
+    }
+}
